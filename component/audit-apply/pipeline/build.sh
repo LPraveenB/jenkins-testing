ff5bf26c64dc55a97c7fb25bf5ec1ee5164c4fc2
LPraveenB <praveen.bl@kloud9.nyc>
2023-10-10 15:48:00 +0530

    Testing

--- component/audit-apply/pipeline/build.sh
+++ component/audit-apply/pipeline/build.sh
@@ -2,7 +2,6 @@
 
 build_json="${WORKSPACE}/component/build.json"
 scripts_dest=$(jq -r .scripts_path.audit_apply "$build_json")
-ls ${WORKSPACE}/component/business-feature/src/*.py
-src_dir="${WORKSPACE}/component/audit-apply/*"
+src_dir="${WORKSPACE}/component/audit-apply/"
 gsutil -m cp -r "$src_dir" "$scripts_dest"
 
