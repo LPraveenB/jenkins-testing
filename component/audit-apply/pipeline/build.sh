2fa230e9bf9b141e456ab0a471dc64d41eeb3b52
LPraveenB <praveen.bl@kloud9.nyc>
2023-10-11 10:06:21 +0530

    Modification

--- component/audit-apply/pipeline/build.sh
+++ component/audit-apply/pipeline/build.sh
@@ -2,6 +2,6 @@
 
 build_json="${WORKSPACE}/component/build.json"
 scripts_dest=$(jq -r .scripts_path.audit_apply "$build_json")
-src_dir="${WORKSPACE}/component/audit-apply"
-gsutil -m cp -r -n "$src_dir" "$scripts_dest"
+src_file="${WORKSPACE}/component/audit-apply/*"
+gsutil -m cp -r "$src_files" "$scripts_dest"
 
