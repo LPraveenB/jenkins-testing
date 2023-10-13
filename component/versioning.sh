dbae360dc0b10b01d3233d15c44c55204937d608
LPraveenB <praveen.bl@kloud9.nyc>
2023-10-13 14:10:21 +0530

    Versioning

---
+++ component/versioning.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Function to generate a version number
+generate_version() {
+  local version=$(date +'%Y%m%d%H')
+  echo "$version"
+}
+
+# Update JSON config with the version
+version=$(generate_version)
+jq --arg ver "$version" '.script_paths.inference_image.imageTAG = $ver' ../build.json > temp.json && mv temp.json ../build.json
